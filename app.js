18231261ff7f285a3ebf90712de184adc720a96b
Karina Gaulin <karina@domandtom.com>
2019-09-20 09:13:24 -0400

    Add logic for genres

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const playStore = require('./playStoreData.js');
 app.use(morgan('common'));
 
 app.get('/apps', (req, res) => {
-  const { sort } = req.query;
+  const { sort, genres } = req.query;
     if(sort) {
       if(!['rating', 'app'].includes(sort)) {
         return res  
@@ -13,12 +13,30 @@ app.get('/apps', (req, res) => {
           .send('Sort must be one of rating or app');
       }
     }
-
     if(sort) {
-      playStore.sort((a, b) => (a.rating > b.raing) ? 1 : ((b.rating > a.rating) ? -1 : 0));
-      console.log(playStore);
+      playStore.sort((a, b) => (a.Rating > b.Rating) ? 1 : ((b.Rating > a.Rating) ? -1 : 0));
+      playStore.sort((a, b) => (a.App > b.App) ? 1 : ((b.App > a.App) ? -1 : 0));
     }
-    res.json(playStore);
+
+    if(genres) {
+      let values = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcat', 'Card'];
+      if(!values.includes(genres)) {
+        return res  
+          .status(400)
+          .send(`Genre must be one of ${values}`);
+      }
+
+      let results = playStore.filter(each => each.Genres === genres);
+      return res.json(results);
+      //playstore is array objects
+      //each object has Genres
+      //genres from request body
+      //filter to see if playStore.Genres === genres
+      //inside filter loop
+    }
+    return res.json(playStore);
+    
+   
   //sort= rating or app, otherwise error, if no value, no sort
   //genres=If genres, values = Action, Puzzle, Strategy, Casual, Arcat, Card
   //genres: otherwise error. Filter list by value given
